05dd76846c00f1e0ec9d3ac4d7db41677d9e7d31
CnrHachi <44537516+ConnorHachi@users.noreply.github.com>
2019-12-26 22:12:53 +0800

    Create 22153.js

---
+++ univ/22153.js
@@ -0,0 +1,21 @@
+var univ_list = {
+    school: "江西理工",                        // 学校名称
+    id: "22153",                               // 学校 ID
+    link: [ {
+        name: "校园主页",                      
+        url: "https://www.jxust.edu.cn/"   
+    }, {
+        name: "教务系统",                      
+        url: "http://jw.jxust.edu.cn/" 
+    }, {
+        name: "图书馆",                       
+        url: "https://tsg.jxust.edu.cn/"
+    },{
+        name: "校园邮箱",                       
+        url: "https://mail.jxust.edu.cn/"
+    },{
+        name: "校园VPN",                       
+        url: "http://webvpn.jxust.edu.cn/users/sign_in/"
+    }
+    ]
+};
